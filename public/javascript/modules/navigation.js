f149add5ff541faaf63a02fac8c01a7bab53b834
Preston Lamb <preston.j.lamb@gmail.com>
2017-09-13 07:34:16 -0600

    webpack fixes; navigation fixes

--- public/javascript/modules/navigation.js
+++ public/javascript/modules/navigation.js
@@ -7,8 +7,16 @@ function openNav(nav) {
 	nav.classList.toggle('navbar--main-nav-open');
 	const body = document.getElementsByTagName('body')[0];
 	body.classList.toggle('navbar--main-nav-open');
-	if (!nav.classList.contains('navbar--main-nav-open')) {
+	if (nav.classList.contains('navbar--main-nav-open')) {
+		body.classList.add('navbar--transitioning');
 		setTimeout(() => {
+			body.classList.remove('navbar--transitioning');
+		}, 700);
+	}
+	if (!nav.classList.contains('navbar--main-nav-open')) {
+		body.classList.add('navbar--transitioning');
+		setTimeout(() => {
+			body.classList.remove('navbar--transitioning');
 			linksContainer.classList.add('navbar__links-container--off-screen');
 		}, 700);
 	}
