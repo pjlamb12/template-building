e14c1f55378276286ab46854506cb34c76db516c
Preston Lamb <preston.j.lamb@gmail.com>
2017-09-13 08:11:00 -0600

    More working on nav fixes

--- public/javascript/modules/navigation.js
+++ public/javascript/modules/navigation.js
@@ -3,17 +3,17 @@ function openNav(nav) {
 	const linksContainer = nav.getElementsByClassName('navbar__links-container')[0];
 
 	linksContainer.classList.remove('navbar__links-container--off-screen');
-	
-	nav.classList.toggle('navbar--main-nav-open');
+
+	nav.classList.toggle('navbar--nav-open');
 	const body = document.getElementsByTagName('body')[0];
-	body.classList.toggle('navbar--main-nav-open');
-	if (nav.classList.contains('navbar--main-nav-open')) {
+	body.classList.toggle('navbar--nav-open');
+	if (nav.classList.contains('navbar--nav-open')) {
 		body.classList.add('navbar--transitioning');
 		setTimeout(() => {
 			body.classList.remove('navbar--transitioning');
 		}, 700);
 	}
-	if (!nav.classList.contains('navbar--main-nav-open')) {
+	if (!nav.classList.contains('navbar--nav-open')) {
 		body.classList.add('navbar--transitioning');
 		setTimeout(() => {
 			body.classList.remove('navbar--transitioning');
